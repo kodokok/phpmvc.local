610950850306b40e95bd8239278feb2f4240d61e
kodokok <sabdo.tri.atmojo@gmail.com>
2019-07-28 00:58:17 +0700

    advance routing
    add simpler but more powerful routes

--- core/Router.php
+++ core/Router.php
@@ -23,8 +23,20 @@ class Router
    * @param string $route The route URL
    * @param array  $params Parameters (conroller, action, etc.)
    */
-  public function add($route, $params)
+  public function add($route, $params = [])
   {
+    // Convert the route to a regular expression: escape forward slashes
+    $route = preg_replace('/\//', '\\/', $route);
+
+    // Convert variable e.g {controller}
+    $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+    // Convert variables with custom regular expressions
+    $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+    // Add start and end delimiters, and case insensitive flag;
+    $route = '/^' . $route . '$/i';
+
     $this->routes[$route] = $params;
   }
 
@@ -48,9 +60,27 @@ class Router
    */
   public function match($url)
   {
-    foreach ($this->routes as $route => $params) {
-      if ($url == $route) {
-        $this->params = $params;
+    // foreach ($this->routes as $route => $params) {
+    //   if ($url == $route) {
+    //     $this->params = $params;
+    //     return true;
+    //   }
+    // }
+
+    // Match to the fixed URL format /controller/action
+    // $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+    foreach ($this->routes as $route => $result) {
+      if (preg_match($route, $url, $matches)) {
+        // get named capture group values
+        // $result = [];
+
+        foreach ($matches as $key => $match) {
+          if (is_string($key)) {
+            $result[$key] = $match;
+          }
+        }
+
+        $this->params = $result;
         return true;
       }
     }
